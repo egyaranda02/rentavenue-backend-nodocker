d135a78813c11b6c2bfbd1f62c97304c488b0a74
egyaranda02 <egyaranda02@gmail.com>
2021-11-09 13:15:02 +0700

    Update Port

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express()
 
 const apiRoutes = require("./routes/index");
 
+const port = (process.env.PORT || 5000);
+
 const transactionExpiration = cron.schedule('* * * * *', async () => {
     console.log('Checking Transactions Expiration');
     const transactions = await db.Transaction.findAll({
