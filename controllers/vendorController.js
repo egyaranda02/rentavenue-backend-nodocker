7e5e8958c5e411dbaab21ce1b7a9699099e6bd7c
egyaranda02 <egyaranda02@gmail.com>
2021-11-11 09:13:33 +0700

    Update cookie samesite to none on login vendor

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -226,7 +226,7 @@ module.exports.login = async function (req, res) {
             const passwordAuth = bcrypt.compareSync(req.body.password, vendor.password);
             if (passwordAuth) {
                 const token = await jwt.sign({ VendorId: vendor.id }, process.env.SECRET_KEY, { expiresIn: tokenAge });
-                res.cookie("jwt", token, { maxAge: 60 * 60 * 1000, httpOnly: false, secure: true })
+                res.cookie("jwt", token, { maxAge: 60 * 60 * 1000, httpOnly: true, secure: true, sameSite: 'none', })
                 return res.status(200).json({
                     success: true,
                     message: "Login Success",
