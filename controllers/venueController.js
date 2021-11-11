f6e10a52a80297900e9a11049c3623411869b00e
egyaranda02 <egyaranda02@gmail.com>
2021-11-11 13:22:03 +0700

    fix dateArray

--- controllers/venueController.js
+++ controllers/venueController.js
@@ -125,7 +125,6 @@ module.exports.getVenueDate = async function (req, res) {
             attributes: ['start_book', 'finish_book']
         })
         function getDates(startDate, finishDate, dateArray) {
-            let dateArray = [];
             let currentDate = startDate;
             while (currentDate <= finishDate) {
                 dateArray.push(currentDate);
